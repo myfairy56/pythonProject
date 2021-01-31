a28750103a1548c49b3bed6eb2d5a96eea5b1cb4
myfairy56 <myfairy56@naver.com>
2021-01-31 23:58:52 +0900

    commit

---
+++ Chapter2/06_type_function.py
@@ -0,0 +1,11 @@
+#type 함수 - 어떤 자료형인지 확인하기 위해 사용
+print(type(3))
+print(type(3.0))
+print(type("3"))
+print(type("True"))
+print(type(True))
+
+def hello():
+    print("Hello world")
+print(type(hello)) #함수
+print(type(print)) #파이썬에 기본적으로 내장되어 있는 함수
