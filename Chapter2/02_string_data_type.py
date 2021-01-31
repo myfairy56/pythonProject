2a541ab511199c3d15d57946193907c4fcce3665
myfairy56 <myfairy56@naver.com>
2021-01-31 19:01:22 +0900

    commit

---
+++ Chapter2/02_string_data_type.py
@@ -0,0 +1,16 @@
+#문자열 - 키보드로 쓸 수 있는 문자들을 표현하는 자료형
+#'코드잇' "코드잇" 동일한 문자열
+print("코드잇")
+print("김남준")
+#print('I'm excited to learn Python!') #I를 문자열로 인식하고 뒤를 인식 못하는 것
+print("I\'m \"excited\" to learn Python!") #따옴표 앞에 \를 넣어서 따옴표를 의도했다고 알려줌
+
+#문자열 연결(String Concatenation)
+print("Hello" + "World")
+print("Hello" * 3)
+print("3"+"5")
+
+#실습과제
+print("'응답하라 1988'은 많은 시청자들에게 사랑을 받은 드라마예요.")
+print('데카르트는 "나는 생각한다. 고로 존재한다." 라고 말했다.')
+print("영화 \'신세계\'에서 \"드루와~\"라는 대사가 유행했다.")
