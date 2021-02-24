90cac4c979369b9c1481df9281da380452f46ece
myfairy56 <myfairy56@naver.com>
2021-02-25 00:20:36 +0900

    commit

---
+++ Chapter2/abstraction/08_style.py
@@ -0,0 +1,32 @@
+print(6.28 * 4)
+print(3.14 * 4 * 4)
+print(6.28 * 8)
+print(3.14 *8 *8)
+'''
+PI = 3.14
+
+radius = 4 #반지름
+print(2 * PI * radius)
+print(PI * radius * radius)
+
+radius=8
+print( 2 * PI * radius)
+print(PI * radius * radius)
+'''
+PI = 3.14
+
+#반지름이 r인 원의 둘레 계산
+def calculate_circumference(r):
+    return 2 * PI * r
+
+#반지름이 r인 원의 넓이 계산
+def calculate_area(r):
+    return PI * r * r
+
+radius = 4
+print(calculate_circumference((radius)))
+print(calculate_area((radius)))
+
+radius = 8
+print(calculate_circumference(radius))
+print(calculate_area(radius))
