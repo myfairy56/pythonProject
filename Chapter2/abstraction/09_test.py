60062a7a75991c87eb0b6aa224f5b3832890e271
myfairy56 <myfairy56@naver.com>
2021-03-06 18:59:23 +0900

    commit

---
+++ Chapter2/abstraction/09_test.py
@@ -0,0 +1,9 @@
+def is_evenly_divisible(number):
+    return number%2 == 0
+
+#테스트
+print(is_evenly_divisible(3))
+print(is_evenly_divisible(7))
+print(is_evenly_divisible(8))
+print(is_evenly_divisible(218))
+print(is_evenly_divisible(317))
